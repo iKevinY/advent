1dea5d0dbcf2a844f8c122ed6cce5a75104f7def
Kevin Yap <me@kevinyap.ca>
2016-12-03 21:31:03 -0800

    Add solution to 2016/04

---
+++ 2016/day04.py
@@ -0,0 +1,33 @@
+import re
+import fileinput
+from collections import Counter
+
+
+def decrypt(c, n):
+    if c == '-':
+        return ' '
+
+    return chr((((ord(c) - ord('a')) + n) % 26) + ord('a'))
+
+
+total_sector_id = 0
+north_pole_sector_id = None
+
+for line in fileinput.input():
+    name, sector, checksum = re.match(r'(\S+)-(\d+)\[(\w{5})\]', line).groups()
+    sector = int(sector)
+
+    real_name = ''.join(decrypt(c, sector) for c in name)
+
+    if 'northpole' in real_name:
+        north_pole_sector_id = sector
+
+    occurences = Counter([x for x in name if x.isalpha()])
+    commons = sorted(occurences.most_common(), key=lambda (x, y): (-y, x))
+
+    if ''.join(zip(*commons)[0][:5]) == checksum:
+        total_sector_id += sector
+
+
+print "Sum of sector IDs of real rooms: %i" % total_sector_id
+print "North Pole storage room sector: %i" % north_pole_sector_id
