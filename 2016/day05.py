d974201f6cba52925a2e9d3ec2811de7e8ef439c
Kevin Yap <me@kevinyap.ca>
2016-12-05 15:51:44 -0800

    Update day05.py

--- 2016/day05.py
+++ 2016/day05.py
@@ -5,27 +5,27 @@ door_id = fileinput.input()[0].strip()
 
 print "Using door ID: %s" % door_id
 
-old_password = ''
-new_password = [None for _ in range(8)]
+pass_1 = ''
+pass_2 = [None for _ in range(8)]
 
 i = 0
 
-while len(old_password) < 8 or None in new_password:
+while len(pass_1) < 8 or None in pass_2:
     digest = md5(door_id + str(i)).hexdigest()
 
     if digest.startswith('00000'):
-        print "Hash found! %s (%i)" % (digest, i)
+        print "Hash found! %s (%08i)" % (digest, i)
 
-        if len(old_password) < 8:
-            old_password += digest[5]
+        if len(pass_1) < 8:
+            pass_1 += digest[5]
 
         pos = int(digest[5], 16)
         char = digest[6]
 
-        if pos < 8 and new_password[pos] is None:
-            new_password[pos] = char
+        if pos < 8 and pass_2[pos] is None:
+            pass_2[pos] = char
 
     i += 1
 
-print "First password is: %s" % old_password
-print "Second password is: %s" % ''.join(new_password)
+print "Password 1 is: %s" % pass_1
+print "Password 2 is: %s" % ''.join(pass_2)
