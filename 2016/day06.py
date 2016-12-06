96d9a03e37e2c627862581d621191ef8c071b652
Kevin Yap <me@kevinyap.ca>
2016-12-05 22:39:46 -0800

    Update day06.py

--- 2016/day06.py
+++ 2016/day06.py
@@ -2,10 +2,8 @@ import fileinput
 from collections import Counter
 
 messages = [line.strip() for line in fileinput.input()]
-columns = []
+message_1 = ''.join(Counter(c).most_common()[0][0] for c in zip(*messages))
+message_2 = ''.join(Counter(c).most_common()[-1][0] for c in zip(*messages))
 
-for i in range(len(messages[0])):
-    columns.append(Counter(msg[i] for msg in messages).most_common())
-
-print "Error-corrected message: %s" % ''.join(c[0][0] for c in columns)
-print "Santa's original message: %s" % ''.join(c[-1][0] for c in columns)
+print "Error-corrected message: %s" % message_1
+print "Santa's original message: %s" % message_2
