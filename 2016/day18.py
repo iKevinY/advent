8d3ca31b95c756fb6cf3abefaf027f9df7797dc9
Kevin Yap <me@kevinyap.ca>
2016-12-18 00:21:06 -0800

    Update day18.py

--- 2016/day18.py
+++ 2016/day18.py
@@ -1,33 +1,34 @@
 import fileinput
 
 
-def is_safe(row, i):
+def is_trap(row, i):
     """Helper function to read out-of-bounds tiles as safe."""
     if 0 <= i < len(row):
         return row[i]
 
-    return True
+    return False
 
 
 def predict_next_row(row):
     """Given a row, returns the predicted following row."""
-    return [not (is_safe(row, i-1) ^ is_safe(row, i+1)) for i in range(len(row))]
+    return [is_trap(row, i-1) ^ is_trap(row, i+1) for i in range(len(row))]
 
 
 if __name__ == '__main__':
-    # Represent safe tiles as True and traps as False
-    raw_first_row = fileinput.input()[0].strip()
-    row = tuple(True if c == '.' else False for c in raw_first_row)
-    safe_tiles = sum(row)
+    # Represent traps as True and safe tiles as False.
+    # The number of safe tiles per row is len(row) - sum(row).
+    row = [True if c == '^' else False for c in fileinput.input()[0].strip()]
+    row_len = len(row)
+    safe_tiles = row_len - sum(row)
 
     for _ in range(40 - 1):
         row = predict_next_row(row)
-        safe_tiles += sum(row)
+        safe_tiles += row_len - sum(row)
 
     print "Number of safe tiles in first 40 rows:", safe_tiles
 
     for _ in range(400000 - 40):
         row = predict_next_row(row)
-        safe_tiles += sum(row)
+        safe_tiles += row_len - sum(row)
 
     print "Number of safe tiles in all 400000 rows:", safe_tiles
