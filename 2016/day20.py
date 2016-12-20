2114ad1db81911051a804094003b03c82dbdd198
Kevin Yap <me@kevinyap.ca>
2016-12-19 21:38:53 -0800

    Update day20.py

--- 2016/day20.py
+++ 2016/day20.py
@@ -8,21 +8,17 @@ def allowed_ips(blacklist, max_ip=MAX_IP):
     """Generator over all allowed IPs given a blacklist."""
     i = 0
 
-    while i <= max_ip:
-        if not blacklist:
-            for n in range(i, max_ip + 1):
-                yield n
-
-            return
-
-        start, end = blacklist.pop(0)
-
+    for start, end in sorted(blacklist):
         if i < start:
             for n in range(i, start):
                 yield n
 
         if i <= end:
             i = end + 1
+
+    if i <= max_ip:
+        for n in range(i, max_ip + 1):
+            yield n
 
 
 if __name__ == '__main__':
@@ -31,8 +27,6 @@ if __name__ == '__main__':
     for line in fileinput.input():
         start, end = parse_line(r'(\d+)-(\d+)', line)
         blacklist.append((start, end))
-
-    blacklist.sort()
 
     whitelist = list(allowed_ips(blacklist))
 
