d25d9e9067a9aa10edcc92012c47306391f7c2be
Kevin Yap <me@kevinyap.ca>
2016-12-23 23:57:45 -0800

    Update day24.py

--- 2016/day24.py
+++ 2016/day24.py
@@ -18,16 +18,16 @@ def valid(grid, x, y):
 
 
 def bfs(grid, start, goal):
-    horizon = deque()
-    horizon.append((start, 0))
+    horizon = deque([(start, 0)])
     seen = set(start)
 
     while horizon:
         (x, y), dst = horizon.pop()
+
         if (x, y) == goal:
             return dst
 
-        for dx, dy, in DIRS:
+        for dx, dy in DIRS:
             nx, ny = x + dx, y + dy
             if (nx, ny) not in seen and valid(grid, nx, ny):
                 horizon.appendleft(((nx, ny), dst + 1))
@@ -47,24 +47,20 @@ def navigate(distances, path, complete=False):
 
 if __name__ == '__main__':
     grid = []
-    num_locs = {}
+    locations = {}
 
     for y, line in enumerate(fileinput.input()):
-        row = []
         for x, tile in enumerate(line):
-            row.append(tile)
             if tile.isdigit():
-                num_locs[int(tile)] = (x, y)
+                locations[int(tile)] = (x, y)
 
-        grid.append(row)
+        grid.append(list(line))
 
     distances = {}
-    max_num = max(num_locs)
 
-    for a, b in combinations(range(max(num_locs) + 1), 2):
-        dst = bfs(grid, num_locs[a], num_locs[b])
-        distances[a, b] = distances[b, a] = dst
+    for a, b in combinations(range(max(locations) + 1), 2):
+        distances[a, b] = distances[b, a] = bfs(grid, locations[a], locations[b])
 
-    paths = list(permutations(range(1, max_num + 1)))
+    paths = list(permutations(range(1, max(locations) + 1)))
     print "Fewest steps to visit all numbers:", min(navigate(distances, path) for path in paths)
     print "Fewest steps to also return to 0:", min(navigate(distances, path, complete=True) for path in paths)
