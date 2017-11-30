94b2559cdc1536ce487f499a6dafe1d09033056f
Kevin Yap <me@kevinyap.ca>
2017-11-29 22:13:27 -0800

    Add starter.py and utils.py for 2017

---
+++ 2017/starter.py
@@ -0,0 +1,31 @@
+import os  # NOQA
+import sys  # NOQA
+import re  # NOQA
+import math  # NOQA
+import fileinput
+from collections import Counter, deque, namedtuple  # NOQA
+from itertools import count, product, permutations, combinations, combinations_with_replacement  # NOQA
+
+from utils import parse_line, mul, factors, memoize, primes, new_table, Point, DIRS, DIRS_4, DIRS_8  # NOQA
+
+# Itertools Functions:
+# product('ABCD', repeat=2)                   AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
+# permutations('ABCD', 2)                     AB AC AD BA BC BD CA CB CD DA DB DC
+# combinations('ABCD', 2)                     AB AC AD BC BD CD
+# combinations_with_replacement('ABCD', 2)    AA AB AC AD BB BC BD CC CD DD
+
+total = 0
+result = []
+table = new_table(None, width=2, height=4)
+
+for i, line in enumerate(fileinput.input()):
+    line = line.strip()
+
+    # data = [x for x in line.split(', ')]
+    # data = [x for x in line]
+    # data = [int(x) for x in line.split()]
+    # data = re.findall(r'(\w+)', line)
+    data = parse_line(r'', line)
+
+    if i == 0:
+        print(data)
---
+++ 2017/utils.py
@@ -0,0 +1,150 @@
+import re
+import math
+import operator
+from functools import total_ordering
+
+
+LETTERS = [x for x in 'abcdefghijklmnopqrstuvwxyz']
+VOWELS = {'a', 'e', 'i', 'o', 'u'}
+CONSONANTS = set(x for x in LETTERS if x not in VOWELS)
+
+
+def parse_line(regex, line):
+    ret = []
+    for match in re.match(regex, line).groups():
+        try:
+            ret.append(int(match))
+        except ValueError:
+            ret.append(match)
+
+    return ret
+
+
+def new_table(val, width, height):
+    return [[val for _ in range(width)] for _ in range(height)]
+
+
+def mul(lst):
+    """Like sum(), but for multiplication."""
+    return reduce(operator.mul, lst, 1)  # NOQA
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def factors(n):
+    """Returns the factors of n."""
+    return sorted(
+        x for tup in (
+            [i, n // i] for i in range(1, int(n ** 0.5) + 1)
+            if n % i == 0)
+        for x in tup)
+
+
+def memoize(f):
+    """Simple dictionary-based memoization decorator"""
+    cache = {}
+
+    def _mem_fn(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
+
+    _mem_fn.cache = cache
+    return _mem_fn
+
+
+def _eratosthenes(n):
+    """http://stackoverflow.com/a/3941967/239076"""
+    # Initialize list of primes
+    _primes = [True] * n
+
+    # Set 0 and 1 to non-prime
+    _primes[0] = _primes[1] = False
+
+    for i, is_prime in enumerate(_primes):
+        if is_prime:
+            yield i
+
+            # Mark factors as non-prime
+            for n in xrange(i * i, n, i):  # NOQA
+                _primes[n] = False
+
+
+def primes(n):
+    """Return a list of primes from [2, n)"""
+    return list(_eratosthenes(n))
+
+
+@total_ordering
+class Point:
+    """Simple 2-dimensional point."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, n):
+        return Point(self.x * n, self.y * n)
+
+    def __div__(self, n):
+        return Point(self.x / n, self.y / n)
+
+    def __neg__(self):
+        return Point(-self.x, -self.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        return self.manhattan < other.manhattan
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+    def __hash__(self):
+        return hash(tuple((self.x, self.y)))
+
+    @property
+    def manhattan(self):
+        return abs(self.x) + abs(self.y)
+
+    @property
+    def distance(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def neighbours_4(self):
+        return [self + p for p in DIRS_4]
+
+    def neighbours_8(self):
+        return [self + p for p in DIRS_8]
+
+
+DIRS_4 = DIRS = [
+    Point(0, 1),   # north
+    Point(1, 0),   # east
+    Point(0, -1),  # south
+    Point(-1, 0),  # west
+]
+
+DIRS_8 = [
+    Point(0, 1),    # N
+    Point(1, 1),    # NE
+    Point(1, 0),    # E
+    Point(1, -1),   # SE
+    Point(0, -1),   # S
+    Point(-1, -1),  # SW
+    Point(-1, 0),   # W
+    Point(-1, 0),   # NW
+]
