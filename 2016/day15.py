3daf8217d47f0a6bc48530efa9445d93ca82c18b
Kevin Yap <me@kevinyap.ca>
2017-11-30 21:13:10 -0800

    Update 2016/day15.py
    Add numerical solution using Chinese remainder theorem.

--- 2016/day15.py
+++ 2016/day15.py
@@ -1,6 +1,6 @@
 import re
 import fileinput
-from utils import parse_line
+from utils import parse_line, mul
 
 
 def button_timing(discs):
@@ -20,6 +20,33 @@ def button_timing(discs):
 
         i += stride
 
+# https://stackoverflow.com/a/9758173/239076
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+def chinese_remainder_theorem(discs):
+    M = mul(d[0] for d in discs)
+    x = 0
+
+    for i, (size, initial) in enumerate(discs, start=1):
+        # Disc #2 has 17 positions; at time=0, it is at position 15.
+        # => x \equiv (17 - 15 - 2) (mod 17)
+        M_i = (M / size)
+        x += (size - initial - i) *  M_i * modinv(M_i, size)
+
+    return x
+
 
 DISCS = []
 DISC_RE = re.compile(r'Disc #\d+ has (\d+) positions; at time=0, it is at position (\d+).')
@@ -28,5 +55,7 @@ for line in fileinput.input():
     disc = parse_line(DISC_RE, line.strip())
     DISCS.append(disc)
 
-print "Timing to press button:", button_timing(DISCS)
-print "Timing with added disc:", button_timing(DISCS + [[11, 0]])
+# print "Timing to press button:", button_timing(DISCS)
+# print "Timing with added disc:", button_timing(DISCS + [[11, 0]])
+print "Timing to press button:", chinese_remainder_theorem(DISCS)
+print "Timing with added disc:", chinese_remainder_theorem(DISCS + [[11, 0]])
