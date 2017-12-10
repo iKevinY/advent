f57b079b4a692100f0c8e401df365c2a6ed554fa
Kevin Yap <me@kevinyap.ca>
2017-12-09 17:09:05 -0800

    Update 2015/day20.py
    Cache previous calls to factors() function.

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,23 +1,30 @@
 import fileinput
 
 PRESENTS = int(fileinput.input()[0].strip())
+CACHE = {}
+
 
 def factors(n):
-    return set(x for tup in ([i, n//i]
-                for i in range(1, int(n**0.5)+1) if n % i == 0) for x in tup)
+    if n in CACHE:
+        return CACHE[n]
+
+    CACHE[n] = set()
+
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            CACHE[n] |= set([i, n//i])
+
+    return CACHE[n]
+
 
 house = 1
-
-while True:
-    if sum(factors(house)) * 10 >= PRESENTS:
-        print "Lowest house number #1:", house
-        break
+while sum(factors(house)) * 10 < PRESENTS:
     house += 1
+
+print "Lowest house number #1:", house
 
 house = 1
-
-while True:
-    if sum(x for x in factors(house) if (x * 50 >= house)) * 11 >= PRESENTS:
-        print "Lowest house number #2:", house
-        break
+while sum(x for x in factors(house) if (x * 50 >= house)) * 11 < PRESENTS:
     house += 1
+
+print "Lowest house number #2:", house
