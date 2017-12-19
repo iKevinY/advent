757b21826a8bb8d3925062ef1c0e7c23a1301cdc
Kevin Yap <me@kevinyap.ca>
2017-12-18 20:31:42 -0800

    Update 2017/day18.py
    The two programs in Part 2 don't need to run in lockstep, so we only
    actually need to yield at `rcv` instructions (each program can execute
    independently until it gets stuck waiting to receive a value). This
    means that no special logic needs to be introduced for Part 1.

--- 2017/day18.py
+++ 2017/day18.py
@@ -2,12 +2,16 @@ import fileinput
 from collections import deque, defaultdict
 
 
-def duet_program(ops, regs, snd_q, rcv_q, part_1=False):
+def duet_program(instrs, snd_q, rcv_q, pid=None):
+    regs = defaultdict(int)
+    if pid is not None:
+        regs['p'] = pid
+
     pc = 0
     sends = 0
 
-    while 0 <= pc < len(ops):
-        op, x, y = ops[pc]
+    while 0 <= pc < len(instrs):
+        op, x, y = instrs[pc]
         y = regs[y] if y.isalpha() else int(y)
 
         if op == 'snd':
@@ -22,60 +26,47 @@ def duet_program(ops, regs, snd_q, rcv_q, part_1=False):
         elif op == 'mod':
             regs[x] %= y
         elif op == 'rcv':
-            if part_1:
-                raise StopIteration
+            if len(rcv_q) > 0:
+                regs[x] = rcv_q.pop()
             else:
-                if len(rcv_q) > 0:
-                    regs[x] = rcv_q.pop()
-                else:
-                    yield op, sends
-                    continue
+                yield op, sends
+                continue
         elif op == 'jgz':
             if (regs[x] if x.isalpha() else int(x)) > 0:
                 pc += y
-                yield op, sends
                 continue
 
         pc += 1
-        yield op, sends
 
     raise StopIteration
 
 
 # Read puzzle input
-OPS = []
+INSTRS = []
 
 for line in fileinput.input():
-    parts = line.strip().split()
-    op, x = parts[:2]
-    y = 'foo' if len(parts) == 2 else parts[2]
-    OPS.append((op, x, y))
+    instr = line.strip() + ' foo'
+    op, x, y = instr.split()[:3]
+    INSTRS.append((op, x, y))
 
 
 # Part 1
-regs = defaultdict(int)
 snd_queue = deque()
-
-for _ in duet_program(OPS, regs, snd_queue, deque(), part_1=True):
-    pass
+program = duet_program(INSTRS, snd_queue, deque())
+next(program)
 
 print "Value of recovered frequency:", snd_queue[0]
 
 
 # Part 2
-regs_0 = defaultdict(int)
-regs_1 = defaultdict(int)
-regs_0['p'] = 0
-regs_1['p'] = 1
-
 queue_0 = deque()
 queue_1 = deque()
 
+program_0 = duet_program(INSTRS, queue_1, queue_0, pid=0)
+program_1 = duet_program(INSTRS, queue_0, queue_1, pid=1)
+
 last_op_0 = None
 last_op_1 = None
-
-program_0 = duet_program(OPS, regs_0, queue_1, queue_0)
-program_1 = duet_program(OPS, regs_1, queue_0, queue_1)
 
 while not (last_op_0 == last_op_1 == 'rcv' and len(queue_0) == len(queue_1) == 0):
     last_op_0, _ = next(program_0)
