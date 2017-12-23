7ea6cfc308990121f133b69f94c60c23adc29450
Kevin Yap <me@kevinyap.ca>
2017-12-22 16:29:13 -0800

    Refactor 2017/21 solution
    Use dictionary to track each grid sequence.

--- 2017/day21.py
+++ 2017/day21.py
@@ -36,14 +36,11 @@ SUBGRIDS['.#...####'] += 1
 for it in range(18):
     # Build up our list of subgrids
     if it % 3 == 0:
-        GRIDS = []
-        ORIG_GRIDS = []
-        for inp in SUBGRIDS:
-            GRIDS.append([list(inp[n:n+3]) for n in range(0, 9, 3)])
-            ORIG_GRIDS.append(inp)
+        GRIDS = {}
+        for key in SUBGRIDS:
+            GRIDS[key] = [list(key[n:n+3]) for n in range(0, 9, 3)]
 
-    NEW_GRIDS = []
-    for grid in GRIDS:
+    for key, grid in GRIDS.items():
         size = len(grid)
         d = 2 if size % 2 == 0 else 3
 
@@ -53,36 +50,36 @@ for it in range(18):
         for y in range(size // d):
             for x in range(size // d):
                 # Build the string representation of each 2x2 or 3x3 subgrid
-                subgrid = ''
-
-                for k in range(d):
-                    subgrid += ''.join(grid[y*d + k][x*d:(x+1)*d])
+                subgrid = ''.join(
+                    ''.join(grid[y*d + k][x*d:(x+1)*d])
+                    for k in range(d)
+                )
 
                 # Lookup the corresponding rule and populate the new grid
-                out = RULES[subgrid]
+                rule = RULES[subgrid]
+
                 for ny in range(d+1):
                     for nx in range(d+1):
-                        new_grid[y*(d+1) + ny][x*(d+1) + nx] = out[ny*(d+1) + nx]
+                        new_grid[y*(d+1) + ny][x*(d+1) + nx] = rule[ny*(d+1) + nx]
 
-        NEW_GRIDS.append(new_grid)
+        GRIDS[key] = new_grid
 
-    GRIDS = NEW_GRIDS
-
+    # Print puzzle output before updating subgrid count
     if it in (4, 17):
-        num_pixels = sum(lit_pixels(g) * SUBGRIDS[ORIG_GRIDS[i]] for i, g in enumerate(GRIDS))
-        print "Lit pixels after {} iterations:".format(it + 1), num_pixels
+        num_pixels = sum(lit_pixels(grid) * SUBGRIDS[key] for key, grid in GRIDS.items())
+        print "Lit pixels after {} iterations: {}".format(it + 1, num_pixels)
 
     # Update the count of independent 3x3 subgrids
     if it % 3 == 2:
         next_subgrids = Counter()
-        for i, grid in enumerate(GRIDS):
-            mult = SUBGRIDS[ORIG_GRIDS[i]]
+        for grid_key, grid in GRIDS.items():
+            subgrid_count = SUBGRIDS[grid_key]
             for y in range(0, 9, 3):
                 for x in range(0, 9, 3):
                     key = ''.join(
                         ''.join(grid[y+n][x:x+3])
                         for n in range(3)
                     )
-                    next_subgrids[key] += mult
+                    next_subgrids[key] += subgrid_count
 
         SUBGRIDS = next_subgrids
