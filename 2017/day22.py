f23923b037138190988f6862441cb9857f1bd2e8
Kevin Yap <me@kevinyap.ca>
2017-12-25 13:46:56 -0800

    Optimize 2017/22 solution
    Use ints to store current position instead of Point objects. This speeds
    up the grid lookups, reducing runtime from ~50s to ~10s.

--- 2017/day22.py
+++ 2017/day22.py
@@ -2,18 +2,19 @@ import fileinput
 from utils import Point, DIRS
 
 
-def simulate_activity(pos, facing, grid, mapping):
-    node = grid.get(pos, '.')
+def simulate_activity(x, y, facing, grid, mapping):
+    node = grid.get((x, y), '.')
     new_node, df = mapping[node]
 
     # Update node
-    grid[pos] = new_node
+    grid[x, y] = new_node
 
     # Update direction and position
     nf = (facing + df) % 4
-    pos += DIRS[nf]
+    x += DIRS[nf].x
+    y += DIRS[nf].y
 
-    return pos, nf, new_node == '#'
+    return x, y, nf, new_node == '#'
 
 
 # Maps node transformations and turn-direction
@@ -36,32 +37,34 @@ WIDTH = 0
 # Read puzzle input
 for y, line in enumerate(fileinput.input()):
     for x, c in enumerate(line.strip()):
-        GRID[Point(x, y)] = c
+        GRID[x, y] = c
 
     WIDTH = x + 1
     HEIGHT += 1
 
 # Part 1
-pos = Point(WIDTH // 2, HEIGHT // 2)
+x = WIDTH // 2
+y = HEIGHT // 2
 facing = 2
 grid = GRID.copy()
 infections_caused = 0
 
 for _ in range(10000):
-    pos, facing, caused = simulate_activity(pos, facing, grid, PART_1_MAP)
+    x, y, facing, caused = simulate_activity(x, y, facing, grid, PART_1_MAP)
     if caused:
         infections_caused += 1
 
 print "Infections caused in Part 1:", infections_caused
 
 # Part 2
-pos = Point(WIDTH // 2, HEIGHT // 2)
+x = WIDTH // 2
+y = HEIGHT // 2
 facing = 2
 grid = GRID.copy()
 infections_caused = 0
 
 for _ in range(10000000):
-    pos, facing, caused = simulate_activity(pos, facing, grid, PART_2_MAP)
+    x, y, facing, caused = simulate_activity(x, y, facing, grid, PART_2_MAP)
     if caused:
         infections_caused += 1
 
