8c865a054c4b401de116103061390b41ac0aa483
Kevin Yap <me@kevinyap.ca>
2017-12-29 00:55:23 -0800

    Update 2017/23 solution
    Use peephole optimizations to solve Part 2 via emulation.

--- 2017/day23.py
+++ 2017/day23.py
@@ -2,56 +2,79 @@ import math
 import fileinput
 from collections import defaultdict
 
-from utils import parse_line, mul, all_unique, factors, memoize, primes  # NOQA
 
-INSTRS = []
-B = None
+def emulate(instrs, **reg_init):
+    regs = defaultdict(int)
+
+    for k, v in reg_init.items():
+        regs[k] = v
+
+    pc = 0
+
+    while 0 <= pc < len(instrs):
+        op, x, y = instrs[pc]
+        y = regs[y] if y.isalpha() else int(y)
+
+        if op == 'set':
+            regs[x] = y
+        elif op == 'sub':
+            regs[x] -= y
+        elif op == 'mul':
+            regs[x] *= y
+            regs['_muls'] += 1
+        elif op == 'mod':
+            regs[x] %= y
+        elif op == 'sqrt':
+            regs[x] = int(math.sqrt(y)) + 1
+        elif op == 'jnz':
+            if (regs[x] if x.isalpha() else int(x)) != 0:
+                pc += y
+                continue
+
+        pc += 1
+
+    return regs
+
 
 # Read puzzle input
+INSTRS = []
+
 for line in fileinput.input():
     instr = line.strip() + ' foo'
     op, x, y = instr.split()[:3]
     INSTRS.append((op, x, y))
 
-    if fileinput.isfirstline():
-        B = int(y)
-
 # Part 1
-REGS = defaultdict(int)
-pc = 0
-muls = 0
-
-while 0 <= pc < len(INSTRS):
-    op, x, y = INSTRS[pc]
-    y = REGS[y] if y.isalpha() else int(y)
-
-    if op == 'set':
-        REGS[x] = y
-    elif op == 'sub':
-        REGS[x] -= y
-    elif op == 'mul':
-        REGS[x] *= y
-        muls += 1
-    elif op == 'mod':
-        REGS[x] %= y
-    elif op == 'jnz':
-        if (REGS[x] if x.isalpha() else int(x)) != 0:
-            pc += y
-            continue
-
-    pc += 1
-
-print "`mul` instruction invocations:", muls
+print "`mul` instruction invocations:", emulate(INSTRS)['_muls']
 
 # Part 2
-b = (B * 100) + 100000
-c = b + 17000
-h = 0
+#
+# Perform peephole optimizations to speed up execution of Part 2
+# (inspired by https://www.reddit.com/r/adventofcode/comments/7lrjei)
+#
+# Speed up composite check using modulo
+#    set  e 2   ->  set  g b
+#    set  g d   ->  mod  g d
+#    mul  g e   ->  jnz  g 8
+#    sub  g b   ->  set  f 0
+#    jnz  g 2   ->  jnz  1 11
+#    set  f 0   ->  nop
+#    sub  e -1  ->  nop
+#    set  g e   ->  nop
+#    sub  g b   ->  nop
+#    jnz  g -8  ->  nop
+#
+# Only check integers up to sqrt(n)
+#     set  g d   ->  sqrt g b
+#     sub  g b   ->  sub  g d
 
-for n in range(b, c + 1, 17):
-    for i in range(2, int(math.sqrt(n))):
-        if n % i == 0:
-            h += 1
-            break
+INSTRS[10] = ['set', 'g', 'b']
+INSTRS[11] = ['mod', 'g', 'd']
+INSTRS[12] = ['jnz', 'g', '8']
+INSTRS[13] = ['set', 'f', '0']
+INSTRS[14] = ['jnz', '1', '11']
 
-print "Value in register h, ie. primes in [{}, {}]: {}".format(b, c, h)
+INSTRS[21] = ['sqrt', 'g', 'b']
+INSTRS[22] = ['sub', 'g', 'd']
+
+print "Value in register h:", emulate(INSTRS, a=1)['h']
