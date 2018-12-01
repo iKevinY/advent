45205de5f73684eb71a869657046b92dc5b3ba17
Kevin Yap <me@kevinyap.ca>
2018-11-30 21:10:42 -0800

    Add template files for 2018

---
+++ 2018/starter.py
@@ -0,0 +1,29 @@
+import os  # NOQA
+import sys  # NOQA
+import re  # NOQA
+import math  # NOQA
+import fileinput
+from collections import Counter, defaultdict, deque, namedtuple  # NOQA
+from itertools import count, product, permutations, combinations, combinations_with_replacement  # NOQA
+
+from utils import parse_line, mul, all_unique, factors, memoize, primes  # NOQA
+from utils import new_table, transposed, rotated  # NOQA
+from utils import md5, sha256, knot_hash  # NOQA
+from utils import Point, DIRS, DIRS_4, DIRS_8  # NOQA
+
+# Itertools Functions:
+# product('ABCD', repeat=2)                   AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
+# permutations('ABCD', 2)                     AB AC AD BA BC BD CA CB CD DA DB DC
+# combinations('ABCD', 2)                     AB AC AD BC BD CD
+# combinations_with_replacement('ABCD', 2)    AA AB AC AD BB BC BD CC CD DD
+
+total = 0
+result = []
+table = new_table(None, width=2, height=4)
+
+for i, line in enumerate(fileinput.input()):
+    line = line.strip()
+    data = parse_line(r'', line)
+
+    if i == 0:
+        print(data)
