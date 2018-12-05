b06380e940e58b493f517f24704081cba4a428e5
Kevin Yap <me@kevinyap.ca>
2018-12-04 21:49:09 -0800

    Add solution for 2018/05

---
+++ 2018/day05.py
@@ -0,0 +1,28 @@
+import fileinput
+from string import ascii_lowercase, ascii_uppercase
+
+
+def reduce_polymer(poly, omit=None):
+    if omit:
+        poly = poly.replace(omit.upper(), '').replace(omit.lower(), '')
+    else:
+        poly = poly.replace('', '')
+
+    while True:
+        improved = False
+        for (x, X) in zip(ascii_lowercase, ascii_uppercase):
+            new = poly.replace(x + X, '').replace(X + x, '')
+            if len(new) < len(poly):
+                improved = True
+                poly = new
+                break
+
+        if not improved:
+            return len(poly)
+
+
+POLYMER = fileinput.input()[0].strip()
+
+
+print "Units remaining after reaction:", reduce_polymer(POLYMER)
+print "Best length after omitting one:", min(reduce_polymer(POLYMER, x) for x in ascii_lowercase)
