ba54360abbc1d05052367fce97f21971dbb1ff00
Kevin Yap <me@kevinyap.ca>
2018-12-10 23:52:11 -0800

    Update 2018/11 solution
    Hooray for partial sums.

--- 2018/day11.py
+++ 2018/day11.py
@@ -1,46 +1,39 @@
 import fileinput
 
 
+SIZE = 300
+
+
 def power_level(x, y, serial):
-    x += 1
-    y += 1
     rack_id = x + 10
     return (((rack_id * y + serial) * rack_id // 100) % 10) - 5
 
 
-SERIAL = int(fileinput.input()[0])
-SIZE = 300
-GRID = [[power_level(x, y, SERIAL) for x in range(SIZE)] for y in range(SIZE)]
-
-overall_sum = 0
-overall_x = 0
-overall_y = 0
-overall_size = 0
-
-for size in range(3, 16):  # window size of 16 is good enough...right?
-    best_sum = 0
+def power_search(partials, conv_size):
+    best_power = 0
     best_x = 0
     best_y = 0
 
-    for y in range(SIZE - size):
-        for x in range(SIZE - size):
-            total = 0
-            for j in range(size):
-                for i in range(size):
-                    total += GRID[y + j][x + i]
+    for y in range(1, SIZE - conv_size + 1):
+        for x in range(1, SIZE - conv_size + 1):
+            xx = x + conv_size
+            yy = y + conv_size
+            power = PARTIALS[yy][xx] + PARTIALS[y][x] - PARTIALS[y][xx] - PARTIALS[yy][x]
 
-            if total > best_sum:
-                best_sum = total
-                best_x = x
-                best_y = y
+            if power > best_power:
+                best_power = power
+                best_x = x + 1
+                best_y = y + 1
 
-    if size == 3:
-        print "Coordinate of most powerful 3x3 grid: {},{}".format(best_x + 1, best_y + 1)
+    return best_power, best_x, best_y, conv_size,
 
-    if best_sum > overall_sum:
-        overall_sum = best_sum
-        overall_x = best_x
-        overall_y = best_y
-        overall_size = size
 
-print "Identifier of the largest total power: {},{},{}".format(overall_x + 1, overall_y + 1, overall_size)
+SERIAL = int(fileinput.input()[0])
+PARTIALS = [[0 for _ in range(SIZE + 1)] for _ in range(SIZE + 1)]
+
+for y in range(1, SIZE + 1):
+    for x in range(1, SIZE + 1):
+        PARTIALS[y][x] = power_level(x, y, SERIAL) + PARTIALS[y][x-1] + PARTIALS[y-1][x] - PARTIALS[y-1][x-1]
+
+print "Coordinate of most powerful 3x3 grid: {1},{2}".format(*power_search(PARTIALS, 3))
+print "Identifier of the largest total power: {1},{2},{3}".format(*max(power_search(PARTIALS, n) for n in range(1, SIZE)))
