cb9584de6c13fce877433f1eef4612d2d21389f6
Kevin Yap <me@kevinyap.ca>
2019-12-18 21:32:59 -0800

    Add solution for 2019/19

---
+++ 2019/day19.py
@@ -0,0 +1,58 @@
+import sys
+import fileinput
+
+from utils import memoize
+from intcode import emulate
+
+
+@memoize
+def affected(x, y):
+    if x < 0 or y < 0:
+        return 0
+
+    vm = emulate(TAPE, 0, [x, y], seq_input=True)
+    return next(vm)
+
+
+# Read problem input
+TAPE = [int(x) for x in fileinput.input()[0].split(',')]
+TAPE += [0] * 100000
+
+
+part_1 = sum(affected(x, y) for y in range(50) for x in range(50))
+print "Points affected by tractor beam:", part_1
+
+# Arbitrary starting point for my input that skips past the incontinuity
+x = 37
+y = 49
+
+while True:
+    while affected(x, y):
+        y += 1
+
+    assert affected(x, y) == 0
+
+    while not affected(x, y):
+        x += 1
+
+    while affected(x, y + 1):
+        y += 1
+
+    assert affected(x, y) == 1
+    assert affected(x + 1, y) == 1
+    assert affected(x - 1, y) == 0
+    assert affected(x, y + 1) == 0
+    assert affected(x, y - 1) == 1
+
+    if affected(x, y - 99) == 1 and affected(x + 99, y) == 1:
+        for yy in range(y - 99, y + 1):
+            for xx in range(x, x + 100):
+                if not affected(xx, yy):
+                    break
+            else:
+                continue
+
+            break
+        else:
+            print "Point checksum", x * 10000 + (y - 99)
+            sys.exit()
