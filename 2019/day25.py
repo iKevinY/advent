1b76678e66c6ddb0ba800bcdc1b038b61cd0c7ba
Kevin Yap <me@kevinyap.ca>
2019-12-24 22:27:18 -0800

    Add solution for 2019/25

---
+++ 2019/day25.py
@@ -0,0 +1,64 @@
+import sys
+import fileinput
+from collections import deque
+from itertools import combinations
+
+from intcode import emulate
+
+TAS = """\
+south
+take space law space brochure
+south
+
+take mouse
+west
+north
+north
+take wreath
+south
+south
+east
+
+south
+take astrolabe
+south
+take mug
+north
+north
+
+north
+west
+take sand
+north
+take manifold
+south
+west
+take monolith
+west
+"""
+
+items = ['space law space brochure', 'mouse', 'sand', 'wreath', 'manifold', 'astrolabe', 'mug', 'monolith']
+
+for i in range(1, len(items) + 1):
+    for comb in combinations(items, i):
+        for item in items:
+            TAS += "drop {}\n".format(item)
+
+        for c in comb:
+            TAS += "take {}\n".format(c)
+
+        TAS += 'west\n'
+
+
+TAPE = [int(x) for x in fileinput.input()[0].split(',')]
+TAPE += [0] * 10000
+inputs = deque(reversed([ord(c) for c in TAS]))
+
+vm = emulate(TAPE, inputs)
+try:
+    while True:
+        resp = chr(next(vm))
+        sys.stdout.write(resp)
+
+except Exception as e:
+    pass
